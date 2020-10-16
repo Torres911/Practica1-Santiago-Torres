9d2f5d1fa2cb5902b2a85c3d155d12ffedd1207d
Torres911 <33793507+Torres911@users.noreply.github.com>
2020-10-15 23:11:19 -0500

    Primer Commit

---
+++ Codigo/Robot/GestorRobot.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+namespace Codigo.Robot{
+
+    public class GestorRobot : Robot{
+        
+    }
+
+}
---
+++ Codigo/Robot/Robot.cs
@@ -0,0 +1,44 @@
+using System;
+
+
+namespace Codigo.Robot{
+
+    public class Robot{
+
+        #region Properties
+        private string Estado;
+        private int CantAgua;
+        private int CantSemillas;
+
+        #endregion Properties
+
+        #region Getters & Setters
+
+        public string getEstado(){
+            return Estado;
+        }
+
+        public void setEstado(string est){
+            this.Estado = est;
+        }
+
+        public int CantAgua(){
+            return CantAgua;
+        }
+
+        public void setCantAgua(int canta){
+            this.CantAgua = canta;
+        }
+
+        public int getCantSemillas(){
+            return CantSemillas;
+        }
+
+        public void setCantSemillas(int cants){
+            this.CantSemillas = cants;
+        }
+
+        #endregion Getters & Setters
+
+    }
+}
